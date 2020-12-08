84f3d0f6043fbd65cd471ee6adec0cedb434006b
Moises <moisessimoes02@gmail.com>
2020-12-08 17:54:35 -0300

    Personalizando o retorno da validação

---
+++ src/main/java/com/moises/cursomc/resources/exception/ValidationError.java
@@ -0,0 +1,24 @@
+package com.moises.cursomc.resources.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+	
+	public ValidationError(Integer status, String mensagem, Long timeStamp) {
+		super(status, mensagem, timeStamp);
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldname, String mensagem) {
+		
+		errors.add(new FieldMessage(fieldname, mensagem));
+	}
+}
