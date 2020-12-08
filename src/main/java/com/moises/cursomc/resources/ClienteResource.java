b28ad8849e09a11fc2dcba00f64f603fda5dfbb9
Moises <moisessimoes02@gmail.com>
2020-12-08 19:37:01 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/moises/cursomc/resources/ClienteResource.java
+++ src/main/java/com/moises/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.moises.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.moises.cursomc.domain.Cliente;
 import com.moises.cursomc.dto.ClienteDTO;
+import com.moises.cursomc.dto.NovoClienteDTO;
 import com.moises.cursomc.services.ClienteService;
 
 @RestController
@@ -32,6 +35,19 @@ public class ClienteResource {
 		Cliente obj = service.find(id);
 		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> inserir(@Valid @RequestBody NovoClienteDTO objDTO) {
+		
+		Cliente obj = service.fromDTO(objDTO);
+		
+		obj = service.save(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 	
 	
--- src/main/java/com/moises/cursomc/services/ClienteService.java
+++ src/main/java/com/moises/cursomc/services/ClienteService.java
@@ -9,10 +9,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.moises.cursomc.domain.Cidade;
 import com.moises.cursomc.domain.Cliente;
+import com.moises.cursomc.domain.Endereco;
+import com.moises.cursomc.domain.enums.TipoCliente;
 import com.moises.cursomc.dto.ClienteDTO;
+import com.moises.cursomc.dto.NovoClienteDTO;
 import com.moises.cursomc.repositories.ClienteRepository;
+import com.moises.cursomc.repositories.EnderecoRepository;
 import com.moises.cursomc.services.exceptions.DataIntegrityException;
 import com.moises.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -22,6 +28,9 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository clienteRepository;
 	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
 	public Cliente find(Integer id) {
 		
 		Optional<Cliente> obj = clienteRepository.findById(id);
@@ -29,10 +38,17 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id + " Tipo: " + Cliente.class.getName()));
 	}
 	
-	
-	//Ainda falta o metodo de salvar...
-	
-	
+	@Transactional
+	public Cliente save(Cliente obj) {
+		
+		obj.setId(null);
+		
+		obj = clienteRepository.save(obj);
+		
+		enderecoRepository.saveAll(obj.getEnderecos());
+		
+		return obj;
+	}
 	
 	
 	public Cliente update(Cliente obj) {
@@ -81,5 +97,24 @@ public class ClienteService {
 		
 		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
 	}
-
+	
+	
+	public Cliente fromDTO(NovoClienteDTO objDTO) {
+		
+		Cliente cli = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfOuCnpj(), TipoCliente.toEnum(objDTO.getTipo()));
+		
+		Cidade city = new Cidade(objDTO.getCidadeID(), null, null);
+		
+		Endereco end = new Endereco(null, objDTO.getLogradouro(), objDTO.getNumero(), objDTO.getComplemento(), objDTO.getBairro(), objDTO.getCep(), cli, city);
+		
+		cli.getEnderecos().add(end);
+		
+		cli.getTelefones().add(objDTO.getTelefone1());
+		
+		if(objDTO.getTelefone2() != null) cli.getTelefones().add(objDTO.getTelefone2()); //Se o telefone2 nao for null, ele adiciona.
+		
+		if(objDTO.getTelefone3() != null) cli.getTelefones().add(objDTO.getTelefone3()); //Se o telefone3 nao for null, ele adiciona
+		
+		return cli; 
+	}
 }
