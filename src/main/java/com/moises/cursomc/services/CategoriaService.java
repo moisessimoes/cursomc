ce2b8620a4f9f7abe4c8b91e9290b77df119d96e
Moises <moisessimoes02@gmail.com>
2020-12-08 15:40:37 -0300

    Listando as Categorias com DTO

--- src/main/java/com/moises/cursomc/services/CategoriaService.java
+++ src/main/java/com/moises/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.moises.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,5 +48,11 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException dive) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos associados a ela.");
 		}
+	}
+	
+	
+	public List<Categoria> findAll() {
+		
+		return categoriaRepository.findAll();
 	}
 }
