574040204a9ffbdf208165085c507f570241add0
Moises <moisessimoes02@gmail.com>
2020-12-09 11:06:46 -0300

    Validação customizada, e-mail não repetido na inserção de um Cliente

--- src/main/java/com/moises/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/moises/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.moises.cursomc.domain.Cliente;
 import com.moises.cursomc.domain.enums.TipoCliente;
 import com.moises.cursomc.dto.NovoClienteDTO;
+import com.moises.cursomc.repositories.ClienteRepository;
 import com.moises.cursomc.resources.exception.FieldMessage;
 import com.moises.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, NovoClienteDTO> {
 	
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	public void initialize(ClienteInsert ann) {}
 	
@@ -29,12 +35,19 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			listaDeErros.add(new FieldMessage("cpfOuCnpj", "CPF Inválido!"));
 		}
 		
+		
 		if(objDTO.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCodigo()) && !BR.isValidCNPJ(objDTO.getCpfOuCnpj())) {
 			
 			listaDeErros.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido!"));
 		}
 		
 		
+		Cliente aux = clienteRepository.findByEmail(objDTO.getEmail());
+		
+		if(aux != null) {
+			
+			listaDeErros.add(new FieldMessage("email", "E-mail já existente!"));
+		}
 		
 		for (FieldMessage fm : listaDeErros) {
 			
