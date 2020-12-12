53ac7151b490b0ab50e6208274be1bdb29a3b5af
Moises <moisessimoes02@gmail.com>
2020-12-12 16:15:03 -0300

    Implementando autorização

--- src/main/java/com/moises/cursomc/config/SecurityConfig.java
+++ src/main/java/com/moises/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.moises.cursomc.security.JWTAuthenticationFilter;
+import com.moises.cursomc.security.JWTAuthorizationFilter;
 import com.moises.cursomc.security.JWTUtil;
 
 @Configuration
@@ -61,6 +62,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
